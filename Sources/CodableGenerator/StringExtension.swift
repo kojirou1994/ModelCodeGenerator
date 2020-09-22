8abc3cb4781ef9c3d3614a98a385c1dd684bbf0c
kojirou <kojirouhtc@gmail.com>
2020-09-22 23:07:56 +0800

    indent format

--- Sources/CodableGenerator/StringExtension.swift
+++ Sources/CodableGenerator/StringExtension.swift
@@ -1,48 +1,48 @@
 import Foundation
 
 extension String {
-    
-    var firstLowercased: String {
-        if isEmpty {
-            return ""
-        }
-        return first!.lowercased() + dropFirst()
+
+  var firstLowercased: String {
+    if isEmpty {
+      return ""
     }
-    
-    var firstUppercased: String {
-        if isEmpty {
-            return ""
-        }
-        return first!.uppercased() + dropFirst()
+    return first!.lowercased() + dropFirst()
+  }
+
+  var firstUppercased: String {
+    if isEmpty {
+      return ""
     }
-    
-    private func cammelcased() -> String {
-        var result = ""
-        var transform = false
-        for cha in self {
-            if cha == "_" {
-                transform = true
-            } else if transform {
-                result.append(String.init(cha).uppercased())
-                transform = false
-            } else {
-                result.append(cha)
-            }
-        }
-        return result
+    return first!.uppercased() + dropFirst()
+  }
+
+  private func cammelcased() -> String {
+    var result = ""
+    var transform = false
+    for cha in self {
+      if cha == "_" {
+        transform = true
+      } else if transform {
+        result.append(String.init(cha).uppercased())
+        transform = false
+      } else {
+        result.append(cha)
+      }
     }
-    
-    /// long_path -> longPath
-    func lowerCamelcased() -> String {
-        cammelcased().firstLowercased
-    }
-    
-    /// long_path -> LongPath
-    func upperCamelcased() -> String {
-        cammelcased().firstUppercased
-    }
-    
-    func addIndent() -> String {
-        self.components(separatedBy: "\n").map({ "    "+$0 }).joined(separator: "\n")
-    }
+    return result
+  }
+
+  /// long_path -> longPath
+  func lowerCamelcased() -> String {
+    cammelcased().firstLowercased
+  }
+
+  /// long_path -> LongPath
+  func upperCamelcased() -> String {
+    cammelcased().firstUppercased
+  }
+
+  func addIndent() -> String {
+    self.components(separatedBy: "\n").map({ "    "+$0 }).joined(separator: "\n")
+  }
 }
--- Sources/CodableGenerator/main.swift
+++ Sources/CodableGenerator/main.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 guard CommandLine.arguments.count > 1 else {
-    print("No input!")
-    exit(1)
+  print("No input!")
+  exit(1)
 }
 
 for i in 1..<CommandLine.arguments.count {
-    let input = URL.init(fileURLWithPath: CommandLine.arguments[i])
-    let inputFilename = input.lastPathComponent.components(separatedBy: ".")[0]
-    
-    let content = try Data.init(contentsOf: input)
-    let json = try JSONSerialization.jsonObject(with: content, options: .allowFragments)
-    
-    var code = StructComponents(name: inputFilename)
-    code.parse(json: json)
-    try code.structCode.write(to: input.deletingLastPathComponent().appendingPathComponent(inputFilename).appendingPathExtension("swift"), atomically: true, encoding: .utf8)
+  let input = URL.init(fileURLWithPath: CommandLine.arguments[i])
+  let inputFilename = input.lastPathComponent.components(separatedBy: ".")[0]
+
+  let content = try Data.init(contentsOf: input)
+  let json = try JSONSerialization.jsonObject(with: content, options: .allowFragments)
+
+  var code = StructComponents(name: inputFilename)
+  code.parse(json: json)
+  try code.structCode.write(to: input.deletingLastPathComponent().appendingPathComponent(inputFilename).appendingPathExtension("swift"), atomically: true, encoding: .utf8)
 }
