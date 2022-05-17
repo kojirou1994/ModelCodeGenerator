0f850b2494c9fbe034a4d8dfc3392526c15bd630
kojirou <kojirouhtc@gmail.com>
2022-05-18 06:04:11 +0800

    update package manifest

--- Package.swift
+++ Package.swift
@@ -1,21 +1,28 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
   name: "ModelCodeGenerator",
   products: [
-    .library(name: "ModelCodeGenerator", targets: ["ModelCodeGenerator"])
+    .library(name: "ModelCodeGenerator", targets: ["ModelCodeGenerator"]),
+    .executable(name: "ModelCodeGeneratorCli", targets: ["ModelCodeGeneratorCli"]),
   ],
   dependencies: [
+    .package(url: "https://github.com/kojirou1994/YYJSONEncoder.git", branch: "main"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
   ],
   targets: [
     .target(
       name: "ModelCodeGenerator",
       dependencies: []),
-    .target(
+    .executableTarget(
       name: "ModelCodeGeneratorCli",
-      dependencies: ["ModelCodeGenerator"]),
+      dependencies: [
+        "ModelCodeGenerator",
+        .product(name: "JSON", package: "YYJSONEncoder"),
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ]),
     .testTarget(
       name: "ModelCodeGeneratorTests",
       dependencies: ["ModelCodeGenerator"]),
